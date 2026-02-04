3abf676fb279a196fc48c068b90ace32a8f308dd
pushpendra <pushpendraranjan1@gmail.com>
2026-02-04 18:56:18 +0530

    created endpoints for admin

---
+++ backend/auth.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+#from typing import List
+from db import get_db
+from models import Admin
+from schema import AdminLoginResponse, AdminCreate, AdminLogin
+from security import hash_password, verify_password, create_access_token, admin_access,super_admin
+router = APIRouter()
+
+
+
+@router.put("/create-admin")
+def create_admin(admin: AdminCreate ,db: Session = Depends(get_db),current_admin: Admin = Depends(super_admin)):   #
+    existing_user = db.query(Admin).filter(Admin.email == admin.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+
+    hashed_password = hash_password(admin.password)
+
+    new_admin = Admin(
+        name=admin.name,
+        email=admin.email,
+        hashed_password=hashed_password, 
+        level="admin"
+    )
+
+    db.add(new_admin)
+    db.commit()
+    db.refresh(new_admin)
+
+    return {
+        "message":f"{new_admin.name}, is now an admin"
+    }
+
+@router.post("/login-admin", response_model= AdminLoginResponse)
+def login_admin(admin: AdminLogin, db: Session = Depends(get_db)): #,current_admin:Admin = Depends(admin_access)
+    db_admin = db.query(Admin).filter(Admin.email == admin.email).first()
+    if not db_admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(admin.password, db_admin.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+
+    token = create_access_token(
+        admin_id= db_admin.id,
+        level = db_admin.level
+    )
+
+    return {
+        "message": f"Hello {db_admin.name}, {db_admin.level}, welcome back",
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
