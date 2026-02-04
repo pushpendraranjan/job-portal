1a6571f45f97fdad7ad1cb813ae253a02401b12b
pushpendra <pushpendraranjan1@gmail.com>
2026-02-04 19:00:02 +0530

    added auth_routes

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from db import engine
 from models import Base
-from jobs import router as auth_router
+from jobs import router as job_router
+from auth import router as auth_router
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(auth_router)
+app.include_router(
+    job_router,
+    prefix ="/jobs",
+    tags=["Jobs"])
+
+app.include_router(
+    auth_router,
+    prefix="/auth",
+    tags=["Auth"]
+)
 
 @app.get("/")
 def root():
