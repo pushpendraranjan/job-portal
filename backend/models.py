57b1762286e14c7bb09e2f0ed8d426e5a6c591c8
pushpendra <pushpendraranjan1@gmail.com>
2026-02-04 18:49:32 +0530

    updated admin

--- backend/models.py
+++ backend/models.py
@@ -18,3 +18,12 @@ class Job(Base):
     compensation_type = Column(String(50), nullable=False)  
     is_open = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+class Admin(Base):
+    __tablename__ = "admins"
+    id = Column(Integer, primary_key= True, index = True)
+    name = Column(String(50), nullable= False)
+    email = Column(String(50), nullable= False, unique= True)
+    hashed_password = Column(String(255), nullable= False)
+    #role = Column(String(50), nullable= False, default= "admin")
+    level = Column(String(50), nullable=False)
