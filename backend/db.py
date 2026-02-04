92a0e8ad12b5ea7d004385ada26768bf114c78fd
pushpendra <pushpendraranjan1@gmail.com>
2026-02-04 17:05:13 +0530

    created job table

---
+++ backend/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+Base = declarative_base()
+
+
+data = "postgresql://company:root@localhost:5433/job_portal_db"
+
+engine = create_engine(data)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
